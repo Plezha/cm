a042e2563dac19dae0f9d146940c11726f86af77
Plezha <plehanovtemka@gmail.com>
2024-02-28 18:40:22 +0300

    changes

--- CM1/interpol.py
+++ CM1/interpol.py
@@ -21,28 +21,37 @@ poly = lagrange_polynomial(x, y)
 spline = CubicSpline(x, y)
 
 points = np.linspace(0.05, 0.95, 10)
-if 1:
-    # Plot data points
-    plt.scatter(points, [0.5] * 10, color='red', label='Data Points')
+# Plot data points
+plt.scatter(points, [0.5] * 10, color='red', label='Data Points')
 
-    # Plot given function interpolation
-    plt.plot(points, [f(x) for x in points], label='f(x)')
+# Plot given function interpolation
+plt.plot(points, [f(x) for x in points], label='f(x)')
 
-    # Plot Lagrange polynomial interpolation
-    plt.plot(points, [poly(x) for x in points], label='Lagrange Interpolation')
+# Plot Lagrange polynomial interpolation
+plt.plot(points, [poly(x) for x in points], label='Lagrange Interpolation')
 
-    # Plot cubic spline interpolation
-    plt.plot(points, [spline(x) for x in points], label='Cubic Spline Interpolation')
-else:
-    # Plot Lagrange polynomial errors
-    plt.plot(points, [f(x) - poly(x) for x in points], label='1000х Lagrange Errors')
-
-    # Plot cubic spline errors
-    plt.plot(points, [f(x) - spline(x) for x in points], label='Cubic Spline Errors')
+# Plot cubic spline interpolation
+plt.plot(points, [spline(x) for x in points], label='Cubic Spline Interpolation')
 
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
 plt.title('Lagrange vs. Cubic Spline Interpolation')
+plt.grid(True)
+plt.figure()
+
+# Plot data points
+plt.scatter(points, [0.0] * 10, color='red', label='Data Points')
+
+# Plot Lagrange polynomial errors
+plt.plot(points, [1000*(f(x) - poly(x)) for x in points], label='1000х Lagrange Errors')
+
+# Plot cubic spline errors
+plt.plot(points, [f(x) - spline(x) for x in points], label='Cubic Spline Errors')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.title('Lagrange vs. Cubic Spline Interpolation Errors')
 plt.grid(True)
 plt.show()
